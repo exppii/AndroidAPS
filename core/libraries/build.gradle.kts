c019f9f23b01354d55b4aac9c9c9b1fa0bd60b78
exppii <5418088+exppii@users.noreply.github.com>
2025-11-17 22:14:28 +0800

    fix

--- core/libraries/build.gradle.kts
+++ core/libraries/build.gradle.kts
@@ -1,7 +1,4 @@
 // in order to use internet's versions you'd need to enable Jetifier again
 // https://github.com/nightscout/iconify.git
-dependencies {
-    // Using version 2.2.2 from Maven because it supports older Android versions
-    implementation("com.joanzapata.iconify:android-iconify:2.2.2")
-    implementation("com.joanzapata.iconify:android-iconify-fontawesome:2.2.2")
-}
+configurations.create("default")
+artifacts.add("default", file("libs/iconify.aar"))
